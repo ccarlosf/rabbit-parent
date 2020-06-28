84ea8d65fc6cddbb9769a6d0f16def1141f7b5e9
ccarlos <2238513100@qq.com>
2020-06-28 16:31:04 +0800

    (3 4 3-16) 定时任务通用组件封装-1

---
+++ rabbit-task/src/main/java/com/ccarlos/rabbit/task/autoconfigure/JobParserAutoConfigurartion.java
@@ -0,0 +1,39 @@
+package com.ccarlos.rabbit.task.autoconfigure;
+
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: 解析定时任务配置类
+ * @author: ccarloselastic.job.zk
+ * @date: 2020/6/22 22:47
+ */
+@Slf4j
+@Configuration
+@ConditionalOnProperty(prefix = "elastic.job.zk",
+        name = {"namespace", "serverLists"}, matchIfMissing = false)
+@EnableConfigurationProperties(JobZookeeperProperties.class)
+public class JobParserAutoConfigurartion {
+
+    @Bean(initMethod = "init")
+    public ZookeeperRegistryCenter zookeeperRegistryCenter(
+            JobZookeeperProperties jobZookeeperProperties) {
+        ZookeeperConfiguration zkConfig = new ZookeeperConfiguration
+                (jobZookeeperProperties.getServerLists(), jobZookeeperProperties.getNamespace());
+        zkConfig.setBaseSleepTimeMilliseconds(zkConfig.getBaseSleepTimeMilliseconds());
+        zkConfig.setMaxSleepTimeMilliseconds(zkConfig.getMaxSleepTimeMilliseconds());
+        zkConfig.setConnectionTimeoutMilliseconds(zkConfig.getConnectionTimeoutMilliseconds());
+        zkConfig.setSessionTimeoutMilliseconds(zkConfig.getSessionTimeoutMilliseconds());
+        zkConfig.setMaxRetries(zkConfig.getMaxRetries());
+        zkConfig.setDigest(zkConfig.getDigest());
+        log.info("初始化job注册中心配置成功, zkaddress : {}, namespace : {}",
+                jobZookeeperProperties.getServerLists(), jobZookeeperProperties.getNamespace());
+        return new ZookeeperRegistryCenter(zkConfig);
+    }
+
+}
