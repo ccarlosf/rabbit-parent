ee39c5ea64205e3f5c865515f96ff087700d9eba
ccarlos <2238513100@qq.com>
2020-06-30 22:43:32 +0800

    (3 4 3-25) 延迟消息应用与封装_

--- rabbit-test/src/test/java/com/ccarlos/rabbit/producer/test/ApplicationTests.java
+++ rabbit-test/src/test/java/com/ccarlos/rabbit/producer/test/ApplicationTests.java
@@ -41,5 +41,27 @@ public class ApplicationTests {
         }
         Thread.sleep(100000);
     }
+
+    @Test
+    public void testProducerClient2() throws Exception {
+
+        for (int i = 0; i < 1; i++) {
+            String uuiqueId = UUID.randomUUID().toString();
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("name", "张三");
+            attributes.put("age", "18");
+            Message message = new Message(
+                    uuiqueId,
+                    "delay-exchange",
+                    "delay.abc",
+                    attributes,
+                    15000
+            );
+            message.setMessageType(MessageType.RELIANT);
+            message.setDelayMills(5000);
+            producerClient.send(message);
+        }
+        Thread.sleep(100000);
+    }
 }
 
